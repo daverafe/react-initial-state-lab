93c2f6ac39489773ea23a8a07c16779248c89b73
David Rafe <david.rafe@gmail.com>
2021-07-19 13:29:41 -0400

    commit tree f9521043323910893f713e41e5200601292e5745

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,29 @@
+
 // your Bomb code here!
+import React from 'react'
+
+export default class Bomb extends React.Component {
+    constructor(props) {
+        super()
+        this.state = {
+            secondsLeft: props.initialCount
+        }
+    }
+
+    render(){
+        if(this.state.secondsLeft > 0) {
+            return (
+                <div>
+                    <p>{this.state.secondsLeft} seconds left before I go boom!</p>
+                </div>
+            )
+        } else {
+            return (
+                <div>
+                    <p>Boom!</p>
+                </div>
+            )
+        }
+        
+    }
+}
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,21 @@
 // your ImageSlider code here!
+import React from 'react'
+
+export default class ImageSlider extends React.Component {
+    constructor(){
+        super()
+        this.state = {
+            currentSlideIndex: 0
+        }
+    }
+    
+    render(){
+        const currentSlide = this.state.currentSlideIndex
+            return (
+                <div>
+                    <p>I am on slide {currentSlide}</p>
+                </div>
+            )
+       
+    }
+}
